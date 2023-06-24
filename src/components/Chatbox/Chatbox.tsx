7203bd99a9ffdeed6a4b866568155d69b620d4b2
donganh123 <donganh12321@gmail.com>
2023-06-24 12:15:45 +0700

    feat: integrate chat service into chatbox

--- src/components/Chatbox/Chatbox.tsx
+++ src/components/Chatbox/Chatbox.tsx
@@ -1,58 +1,77 @@
 import React, { useState, useEffect, useRef } from "react";
 import Classes from "./Chatbox.module.scss";
+import { Message } from "@/types/response";
+import { ChatService } from "@/services/ChatService";
+import cl from "classnames";
 
-const endpoint = "https://api.openai.com/v1/chat/completions";
-const apiKey = "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";
 export const Chatbox = () => {
-  const [chat, setChat] = useState<{ role: string; content: string }[]>([]);
+  const [chat, setChat] = useState<Message[]>([]);
+  const [message, setMessage] = useState<string>("");
   const chatRef = useRef<HTMLInputElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
   const submitChathandler = async () => {
-    try {
-      setChat((prevChat) => [
-        ...prevChat,
-        { role: "user", content: chatRef?.current?.value as string },
-      ]);
-      const res = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [
-            ...chat,
-            { role: "user", content: chatRef?.current?.value },
-          ],
-        }),
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to get response!");
-      }
-
-      const data: ChatResponse = await res.json();
-      const newMessages = data.choices.map((choice) => ({
-        role: choice.message.role,
-        content: choice.message.content,
-      }));
-      setChat((prevChat) => [...prevChat, ...newMessages]);
-    } catch (error: any) {
-      console.log(error.message);
-    }
+    setMessage(chatRef?.current?.value as string);
+    setChat((prevChat) => [
+      ...prevChat,
+      { role: "user", content: chatRef?.current?.value as string },
+    ]);
   };
+
+  const chatServiceInstance = new ChatService("/v1/chat/completions");
+
+  useEffect(() => {
+    if (chat.length === 0) return;
+    chatRef?.current?.value ? (chatRef.current.value = "") : null;
+    const getMessage = async () => {
+      try {
+        const newMessage = await chatServiceInstance.getMessage(chat);
+        setChat((prevChat) => [
+          ...prevChat,
+          ...newMessage.choices.map((choice) => ({
+            role: choice.message.role,
+            content: choice.message.content,
+          })),
+        ]);
+      } catch (error) {
+        console.error((error as any).message);
+      }
+    };
+
+    getMessage();
+  }, [message]);
+
+  useEffect(() => {
+    console.log(chat);
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [JSON.stringify(chat)]);
 
   return (
     <div className={Classes.Chatbox}>
       <div className={Classes.Chatbox__Header}>Persona</div>
       <div className={Classes.Chatbox__MessagesContainer}>
-        {chat.map((message) => (
-          <div>{message.content}</div>
+        {chat.map((message, index) => (
+          <div
+            className={cl(Classes.Chatbox__Message, {
+              [Classes.Chatbox__UserMessage]: message.role === "user",
+              [Classes.Chatbox__AssistantMessage]: message.role === "assistant",
+            })}
+          >
+            {message.content}
+          </div>
         ))}
+        <div ref={bottomRef}></div>
       </div>
       <div className={Classes.Chatbox__ActionsContainer}>
-        <input className={Classes.Chatbox__Chatbar} ref={chatRef} />
+        <input
+          className={Classes.Chatbox__Chatbar}
+          ref={chatRef}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              submitChathandler();
+            }
+          }}
+        />
         <button className={Classes.SubmitBtn} onClick={submitChathandler}>
           Send
         </button>
